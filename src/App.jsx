ac07417baae215fc7a9e0e6d31b43a2d49b31d42
Pratham <ppratham812@gmail.com>
2023-10-21 14:19:19 +0530

    blogs part developed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Banner from './components/Banner';
+import Blogs from './components/Blogs';
 import NavbarComponent from './components/Nav';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
@@ -11,6 +12,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Blogs />
     </>
   )
 }
---
+++ src/data/blogs.js
@@ -0,0 +1,31 @@
+import blog1 from '../assets/blog1.png';
+import blog2 from '../assets/blog2.png';
+
+const blogs = [{
+        img: blog1,
+        name: 'How Internet Works Internally? All about DNS, CDNs & More',
+        url: 'https://prathampatel.hashnode.dev/how-internet-works-internally-what-is-dns-cdns-more',
+    },
+    {
+        img: blog2,
+        name: 'Coming Soon!! Coming Soon!! Coming Soon!! Coming Soon!!',
+        url: '#',
+    },
+    {
+        img: blog2,
+        name: 'Coming Soon!! Coming Soon!! Coming Soon!! Coming Soon!!',
+        url: '#',
+    },
+    {
+        img: blog2,
+        name: 'Coming Soon!! Coming Soon!! Coming Soon!! Coming Soon!!',
+        url: '#',
+    },
+    {
+        img: blog2,
+        name: 'Coming Soon!! Coming Soon!! Coming Soon!! Coming Soon!!',
+        url: '#',
+    },
+];
+
+export default blogs;

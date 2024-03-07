7abf1f352e7dfaee865d676f51437689f9e19655
Pratham <ppratham812@gmail.com>
2024-03-07 21:54:39 +0530

    feat:Email service developed and integrated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/contact-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import axios from "axios";
 
 export const Contact = () => {
     const formInitialDetails = {
@@ -25,7 +26,14 @@ export const Contact = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
+        setButtonText("Sending...");
+        const response = await axios.post('http://localhost:3000/api/v1/sendmail',formDetails);
+        if (response.data.status!==200){
+            setButtonText('Some Error occurred!! Try again later');
+            return;
+        }
+        setButtonText("Email Sent!!");
+        setFormDetails(formInitialDetails);
     };
 
     return (
@@ -47,19 +55,19 @@ export const Contact = () => {
                                     <form onSubmit={handleSubmit}>
                                         <Row>
                                             <Col size={12} sm={6} className="px-1">
-                                                <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                                                <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} required />
                                             </Col>
                                             <Col size={12} sm={6} className="px-1">
-                                                <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
+                                                <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} required/>
                                             </Col>
                                             <Col size={12} sm={6} className="px-1">
-                                                <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                                                <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} required />
                                             </Col>
                                             <Col size={12} sm={6} className="px-1">
-                                                <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
+                                                <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} required/>
                                             </Col>
                                             <Col size={12} className="px-1">
-                                                <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                                                <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)} required></textarea>
                                                 <button type="submit"><span>{buttonText}</span></button>
                                             </Col>
                                             {
